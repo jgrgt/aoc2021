71fc6ef6867c522e86d4c94ac53fd5181e515cf1
Jens Geiregat <jens.geiregat@axians.com>
2021-12-07 16:48:33 +0100

    Day 7 part 2 done (brute force...)

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -8,6 +8,29 @@ class Day7 : Day(7) {
         val median = horizontalPositions.median()
         return horizontalPositions.sumOf { abs(it - median) }
     }
+
+    override fun runPartTwo(lines: List<String>): Any {
+        val horizontalPositions = lines[0].split(",").map { it.trim().toInt() }
+        val min = horizontalPositions.minOrNull()!!
+        val max = horizontalPositions.maxOrNull()!!
+        val costs = (min..max).map { target ->
+            calculateCost(target, horizontalPositions)
+        }
+        return costs.minOrNull()!!
+    }
+
+    private fun calculateCost(target: Int, horizontalPositions: List<Int>): Int {
+        return horizontalPositions.sumOf { pos ->
+            cost(abs(target - pos))
+        }
+    }
+
+    private fun cost(steps: Int): Int {
+        if (steps == 0) {
+            return 0
+        }
+        return steps + cost(steps - 1)
+    }
 }
 
 fun List<Int>.median(): Int {
