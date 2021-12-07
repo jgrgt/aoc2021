29050c5d43e3e40f93ca11a9e3e6e051e3ab258b
Jens Geiregat <jens.geiregat@axians.com>
2021-12-07 16:40:01 +0100

    Day 7 part 1 done

--- src/main/kotlin/days/Day7.kt
+++ src/main/kotlin/days/Day7.kt
@@ -1,4 +1,17 @@
 package days
 
+import kotlin.math.abs
+
 class Day7 : Day(7) {
+    override fun runPartOne(lines: List<String>): Any {
+        val horizontalPositions = lines[0].split(",").map { it.trim().toInt() }
+        val median = horizontalPositions.median()
+        return horizontalPositions.sumOf { abs(it - median) }
+    }
+}
+
+fun List<Int>.median(): Int {
+    val sorted = this.sorted()
+    // simplified, does not average out if 2 options
+    return sorted[this.size / 2]
 }
